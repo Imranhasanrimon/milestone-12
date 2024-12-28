d16faadb53761ae1fc45f4eb4ce246545dcd32b7
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-28 22:33:12 +0600

    basic router setup

--- bistro-boss-client/src/routes/router.jsx
+++ bistro-boss-client/src/routes/router.jsx
@@ -1,8 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: "/",
+                element: <h1>hello world</h1>
+            }
+        ]
     },
 ]);
