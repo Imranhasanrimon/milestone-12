ba617fb05a3af6f52c573ed740d45cea3798c820
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-29 22:17:23 +0600

    adding swiper in testimonial page

--- bistro-boss-client/src/pages/home/Review.jsx
+++ bistro-boss-client/src/pages/home/Review.jsx
@@ -1,4 +1,13 @@
 import { useEffect, useState } from "react";
+import SectionTitle from "../../components/SectionTitle";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Pagination, Navigation } from 'swiper/modules';
 
 const Review = () => {
     const [review, setReview] = useState([]);
@@ -7,11 +16,30 @@ const Review = () => {
             .then(res => res.json())
             .then(data => setReview(data))
     }, []);
-    console.log(review);
-    return (
-        <div>
 
-        </div>
+    return (
+        <section>
+            <SectionTitle subHeading={'What Our Client Say'} heading={'testimonials'}></SectionTitle>
+            <Swiper
+                pagination={{
+                    type: 'fraction',
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
+
+        </section>
     );
 };
 
