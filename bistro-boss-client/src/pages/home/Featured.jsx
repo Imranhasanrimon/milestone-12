1f9dec2fa561e6f5e206822b94df78e755a7f28e
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-29 22:04:11 +0600

    adding review section

--- bistro-boss-client/src/pages/home/Featured.jsx
+++ bistro-boss-client/src/pages/home/Featured.jsx
@@ -3,15 +3,15 @@ import featuredImg from '../../assets/home/featured.jpg';
 import './featured.css'
 const Featured = () => {
     return (
-        <section className="bg-featured p-5 text-white">
+        <section className="bg-featured bg-fixed p-5 text-white">
             <SectionTitle heading={'featured item'} subHeading={'check it out'}></SectionTitle>
-            <div className="md:flex gap-5 md:px-20 lg:px-40 xl:px-60">
+            <div className="md:flex gap-5 md:px-20 lg:px-40 xl:px-60 bg-slate-400 bg-opacity-40">
                 <img className="md:w-96" src={featuredImg} alt="" />
                 <div>
                     <h5>Aug 20, 2029</h5>
                     <h3 className="uppercase font-semibold">where can i get some?</h3>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, quasi doloremque quis architecto voluptatum soluta doloribus similique at exercitationem veritatis ipsam illo ducimus assumenda delectus dignissimos pariatur. Facilis, nemo qui!</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 text-white border-white">Order Now</button>
                 </div>
             </div>
         </section>
