1c4264bbd7514e3057172a37b17974363f5521c3
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-29 08:23:13 +0600

    Navbar is done

--- bistro-boss-client/src/layouts/MainLayout.jsx
+++ bistro-boss-client/src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/shared/Footer";
+import Navbar from "../pages/shared/Navbar";
 
 const MainLayout = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
