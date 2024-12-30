527bccdc892964f9ce14db3572a2d46785a95365
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-30 12:16:54 +0600

    installing and using react helmet async for dynamic title

--- bistro-boss-client/src/pages/home/Home.jsx
+++ bistro-boss-client/src/pages/home/Home.jsx
@@ -3,10 +3,14 @@ import Category from "./Category";
 import Featured from "./Featured";
 import PopularMenu from "./PopularMenu";
 import Review from "./Review";
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Home</title>
+            </Helmet>
             <Banner></Banner>
             <Category></Category>
             <PopularMenu></PopularMenu>
