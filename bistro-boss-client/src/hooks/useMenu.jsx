6755b4d35f9ed305a23262ca272d62d79e44a7fb
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-31 19:08:43 +0600

    fetching data from database instead of public directiory

--- bistro-boss-client/src/hooks/useMenu.jsx
+++ bistro-boss-client/src/hooks/useMenu.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const useMenu = () => {
     const [items, setItems] = useState([])
     useEffect(() => {
-        fetch('/menu.json')
+        fetch('http://localhost:5000/menu')
             .then(res => res.json())
             .then(data => {
                 setItems(data)
