1452e55f8568d17962b6f6b5c42e56a2cdce6b9c
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-31 22:37:47 +0600

    conditional header and footer based on the login location usiing useLocation hook

--- bistro-boss-client/src/layouts/MainLayout.jsx
+++ bistro-boss-client/src/layouts/MainLayout.jsx
@@ -1,15 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../pages/shared/Footer";
 import Navbar from "../pages/shared/Navbar";
 
 const MainLayout = () => {
+    const location = useLocation();
+    const noNavbarFooter = location.pathname.includes('login');
     return (
         <div>
-            <Navbar></Navbar>
+            {noNavbarFooter || <Navbar></Navbar>}
             <div className="max-w-screen-2xl w-11/12 mx-auto">
                 <Outlet></Outlet>
             </div>
-            <Footer></Footer>
+            {noNavbarFooter || <Footer></Footer>}
         </div>
     );
 };
