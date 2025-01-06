bc17d31c3ccf0229c616b265f026308f0c802718
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-01-06 22:20:26 +0600

    changing the name of router

--- bistro-boss-client/src/pages/dashboard/Dashboard.jsx
+++ bistro-boss-client/src/pages/dashboard/Dashboard.jsx
@@ -2,7 +2,7 @@ import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
     // TODO: isAdmin value should be loaded form database;
-    const isAdmin = true;
+    const isAdmin = false;
 
     return (
         <div className="flex dashboard max-w-screen-2xl w-11/12 mx-auto my-10">
@@ -13,7 +13,7 @@ const Dashboard = () => {
                         <NavLink className='bg-black bg-opacity-10 p-2 rounded-lg hover:bg-opacity-25 ' to='/dashboard/reservation'>Add Items</NavLink>
                         <NavLink className='bg-black bg-opacity-10 p-2 rounded-lg hover:bg-opacity-25 ' to='/dashboard/reservation'>Manage Items</NavLink>
                         <NavLink className='bg-black bg-opacity-10 p-2 rounded-lg hover:bg-opacity-25 ' to='/dashboard/reservation'>Manage Bookings</NavLink>
-                        <NavLink className='bg-black bg-opacity-10 p-2 rounded-lg hover:bg-opacity-25 ' to='/dashboard/cart'>All Users</NavLink>
+                        <NavLink className='bg-black bg-opacity-10 p-2 rounded-lg hover:bg-opacity-25 ' to='/dashboard/allUsers'>All Users</NavLink>
                     </>
                         :
                         <>
