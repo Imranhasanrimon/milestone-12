51976a2a25b3d9f48f818dfea59b33f0823b6ee9
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-01-06 19:27:08 +0600

    google login method is done

--- bistro-boss-client/src/pages/shared/SocialLogin.jsx
+++ bistro-boss-client/src/pages/shared/SocialLogin.jsx
@@ -1,9 +1,17 @@
 import { FaGoogle } from "react-icons/fa";
+import useAuth from './../../hooks/useAuth';
 
 const SocialLogin = () => {
+    const { signInWithGoogle } = useAuth();
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+            .then(result => {
+                console.log(result.user);
+            })
+    }
     return (
         <div>
-            <button className="btn btn-sm w-full bg-success">Google <FaGoogle /></button>
+            <button onClick={handleGoogleSignIn} type="button" className="btn btn-sm w-full bg-success">Google <FaGoogle /></button>
         </div>
     );
 };
--- bistro-boss-client/src/routes/router.jsx
+++ bistro-boss-client/src/routes/router.jsx
@@ -4,10 +4,10 @@ import Home from "../pages/home/Home";
 import Menu from "../pages/menu/Menu";
 import Order from "../pages/order/Order";
 import Login from "../pages/login/Login";
-import SignUp from "../pages/signUp/signUp";
 import PrivateRoute from "./PrivateRoute";
 import Dashboard from "../pages/dashboard/Dashboard";
 import MyCart from "../pages/dashboard/MyCart";
+import SignUp from "../pages/signUp/SignUp";
 
 export const router = createBrowserRouter([
     {
