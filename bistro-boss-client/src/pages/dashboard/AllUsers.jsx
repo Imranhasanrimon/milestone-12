84bfd540b8524e0ebf2e9061c920c11255fc09cc
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-01-07 07:50:26 +0600

    delete operation for users

--- bistro-boss-client/src/pages/dashboard/AllUsers.jsx
+++ bistro-boss-client/src/pages/dashboard/AllUsers.jsx
@@ -1,21 +1,76 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { MdDelete } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
+
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
-    const { data: users } = useQuery({
+    const { data: users, refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users');
             return res.data;
         }
     });
-    console.log(users);
+
+    const handleDelete = (user) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        console.log(res.data.deletedCount);
+                        if (res.data.deletedCount > 0) {
+                            refetch()
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your user has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div>
             <div className="flex justify-between items-center">
                 <h3 className="text-4xl">All Users</h3>
-                <h3 className="text-4xl">Total Users</h3>
+                <h3 className="text-4xl">Total Users: {users?.length}</h3>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users?.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td><FaUsers className="text-3xl" /></td>
+                                <td><MdDelete onClick={() => handleDelete(user)} className="text-3xl text-red-500 cursor-pointer" /></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
             </div>
         </div>
     );
--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -74,6 +74,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result)
+        })
+
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
