11ddc9d7d02cb887a34e5b87fdc7b7b2be78706c
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-01-09 17:05:25 +0600

    adding menu mangement table

--- bistro-boss-client/src/pages/dashboard/ManageItem.jsx
+++ bistro-boss-client/src/pages/dashboard/ManageItem.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import SectionTitle from '../../components/SectionTitle';
+import useMenu from '../../hooks/useMenu';
+import { MdEditSquare, MdDelete } from "react-icons/md";
 
 const ManageItem = () => {
+    const [menu] = useMenu();
     return (
         <div>
             <SectionTitle heading={'manage all items'} subHeading={'Hurry up'}></SectionTitle>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Serial</th>
+                            <th>Image</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Update</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            menu.map((item, index) => <tr key={item._id}>
+                                <th>{index + 1}</th>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle h-12 w-12">
+                                            <img
+                                                src={item.image}
+                                                alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{item.name}</td>
+                                <td>$ {item.price}</td>
+                                <th><button className='btn btn-primary'><MdEditSquare /></button> </th>
+                                <th><button className='btn btn-error'><MdDelete /></button> </th>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
