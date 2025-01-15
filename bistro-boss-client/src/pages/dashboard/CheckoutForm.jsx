d544e2f2830dec0aeab46254889ffeaa9f39a606
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-01-15 16:50:34 +0600

    saving payment history in the database

--- bistro-boss-client/src/pages/dashboard/CheckoutForm.jsx
+++ bistro-boss-client/src/pages/dashboard/CheckoutForm.jsx
@@ -83,6 +83,19 @@ const CheckoutForm = () => {
             if (paymentIntent.status === 'succeeded') {
                 console.log('transaction Id', paymentIntent.id)
                 setTransactionId(paymentIntent.id)
+
+                //now save the payment in the database..
+                const payment = {
+                    email: user.email,
+                    price: totalPrice,
+                    transactionId: paymentIntent.id,
+                    date: new Date(),
+                    cartIds: cart.map(item => item._id),
+                    menuItemIds: cart.map(item => item.itemId),
+                    status: 'pending'
+                }
+                const res = axiosSecure.post('/payments', payment)
+                console.log('payment saved', res);
             }
         }
     }
