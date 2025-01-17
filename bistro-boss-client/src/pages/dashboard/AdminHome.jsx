bb1dec690bc81a9e84298033ef1847157695f378
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-01-17 08:11:09 +0600

    admin homePage is done

--- bistro-boss-client/src/pages/dashboard/AdminHome.jsx
+++ bistro-boss-client/src/pages/dashboard/AdminHome.jsx
@@ -1,10 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { FaDollarSign } from "react-icons/fa";
+import { LuSquareMenu } from "react-icons/lu";
+import { GrDeliver } from "react-icons/gr";
+import { FaUsers } from "react-icons/fa";
 
 const AdminHome = () => {
     const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const { data: stats } = useQuery({
+        queryKey: ['admin-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/admin-stats');
+            return res.data
+        }
+    });
+    console.log(stats);
+
     return (
         <div>
-            <h2>Hi..! Welcome {user ? user.displayName : 'Back'}</h2>
+            <h2 className="text-3xl font-semibold">Hi..! Welcome {user ? user.displayName : 'Back'}</h2>
+
+            <div className="stats shadow border border-gray-600">
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FaDollarSign className="text-3xl" />
+                    </div>
+                    <div className="stat-title">Revenues</div>
+                    <div className="stat-value">{stats?.revenue}</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FaUsers className="text-3xl" />
+                    </div>
+                    <div className="stat-title"> Users</div>
+                    <div className="stat-value">{stats.users}</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <LuSquareMenu className="text-3xl" />
+                    </div>
+                    <div className="stat-title">Menu Items</div>
+                    <div className="stat-value">{stats.menuItems}</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <GrDeliver className="text-3xl" />
+                    </div>
+                    <div className="stat-title">Orders</div>
+                    <div className="stat-value">{stats.payments}</div>
+                </div>
+            </div>
 
         </div>
     );
--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -220,6 +220,27 @@ async function run() {
             res.send({ paymentResult, deleteResult })
         })
 
+        //stats or analytics
+        app.get('/admin-stats', verifyToken, verifyAdmin, async (req, res) => {
+            const users = await userCollection.estimatedDocumentCount();
+            const menuItems = await menuCollection.estimatedDocumentCount();
+            const payments = await paymentCollection.estimatedDocumentCount();
+
+            const result = await paymentCollection.aggregate([
+                {
+                    $group: {
+                        _id: null,
+                        totalRevenue: {
+                            $sum: '$price'
+                        }
+                    }
+                }
+            ]).toArray();
+            const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+            res.send({ users, menuItems, payments, revenue })
+        })
+
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
